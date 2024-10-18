a5c035946105608a804e8ca608cf7ff46d55280d
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-10-18 14:59:35 -0300

    Create project

---
+++ src/main/java/com/cad_escolar/cad_escolar/CadEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.cad_escolar.cad_escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cad_escolar/cad_escolar/CadEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cad_escolar.cad_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
