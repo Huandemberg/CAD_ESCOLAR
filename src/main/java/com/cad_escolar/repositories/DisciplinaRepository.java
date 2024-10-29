b9c1d4429d0d4ce7eebb050f99bb4abb082eba9e
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-10-28 21:55:43 -0300

    annotations

--- src/main/java/com/cad_escolar/repositories/DisciplinaRepository.java
+++ src/main/java/com/cad_escolar/repositories/DisciplinaRepository.java
@@ -1,9 +1,11 @@
 package com.cad_escolar.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cad_escolar.models.Disciplina;
 
+@Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
     
     
--- src/main/java/com/cad_escolar/repositories/NotaRepository.java
+++ src/main/java/com/cad_escolar/repositories/NotaRepository.java
@@ -1,9 +1,11 @@
 package com.cad_escolar.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cad_escolar.models.Nota;
 
+@Repository
 public interface NotaRepository extends JpaRepository<Nota, Long> {
     
 }
